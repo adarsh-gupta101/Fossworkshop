7630cd239b7d2f521660d6a7a80b75072d019bc9
Adarsh <adarshgupta2468@gmail.com>
2021-12-17 23:28:09 +0530

    fooss

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./App.css";
+import Banner from "./components/banner";
+import Header from "./components/header";
+import Searchbar from "./components/searchbar";
+
+
+function App() {
+  return (
+    <div className="">
+
+
+<Header/>
+<Banner/>
+<Searchbar/>
+    </div>
+  );
+}
+
+export default App;
